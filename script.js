dcb1168a3b3c6319591777a6740c45de06973496
Julio Freitas <julio@draftit.com.br>
2023-02-14 20:37:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+    let background = document.querySelector(".background");
+    let section7 = document.querySelector(".section-7");
+  
+    window.addEventListener("scroll", () => {
+      let scrollTop = window.scrollY;
+      let maxScroll = section7.offsetHeight - window.innerHeight;
+      let percent = scrollTop / maxScroll;
+      let movement = percent * (background.offsetWidth - window.innerWidth + window.innerHeight);
+      background.style.transform = `translateX(-${movement}px)`;
+    });
+  });
